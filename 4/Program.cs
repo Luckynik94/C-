118193443e8915d955fd9cdaab0180e9f109ff5c
Luckynik <nikita.mishin.1994@gmail.com>
2023-11-13 10:06:10 +0400

    Save 1

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,101 +1,101 @@
-﻿// Задача 24: Напишите программу, которая
-//  принимает на вход число (А) и выдаёт
-//   сумму чисел от 1 до А.
-// 7 -> 28
-// 4 -> 10
-// 8 -> 36
+﻿// // Задача 24: Напишите программу, которая
+// //  принимает на вход число (А) и выдаёт
+// //   сумму чисел от 1 до А.
+// // 7 -> 28
+// // 4 -> 10
+// // 8 -> 36
 
-// возвращающее значение + Название + ()+ {}
+// // возвращающее значение + Название + ()+ {}
 
-Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-int GetSum()
-{
-    int start = 1;
-    int sum = 0;
-    while (start <= num)
-    {
-        sum += start;
-        start++;
-        // start = start+1;
-    }
-    return sum;
-}
+// int GetSum()
+// {
+//     int start = 1;
+//     int sum = 0;
+//     while (start <= num)
+//     {
+//         sum += start;
+//         start++;
+//         // start = start+1;
+//     }
+//     return sum;
+// }
 
-// int result = GetSum();
-// System.Console.WriteLine(result);
+// // int result = GetSum();
+// // System.Console.WriteLine(result);
 
-Console.WriteLine(GetSum());
+// Console.WriteLine(GetSum());
 
-Задача 26: Напишите программу, которая 
-принимает на вход число и выдаёт количество 
-цифр в числе.
-456 -> 3
-78 -> 2
-89126 -> 5
-
-
-Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine(GetCountNumber(num));
-
-int GetCountNumber(int newNum)
-{
-    int count = 0;
-
-    while (newNum > 0)
-    {
-        newNum = newNum / 10;
-        count++;
-    }
-
-    return count;
-}
-
-Задача 28: Напишите программу, которая принимает
- на вход число N и выдаёт произведение чисел от 1 до N.
-4 -> 24 
-5 -> 120
-
-Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
-
-int GetSum()
-{
-    int start = 1;
-    int multiplication = 1;
-
-    while (start <= num)
-    {
-        multiplication *= start;
-        start++;
-        // start = start+1;
-    }
-    return multiplication;
-}
-
-// int result = GetSum();
-// System.Console.WriteLine(result);
-
-Console.WriteLine(GetSum());
+// Задача 26: Напишите программу, которая 
+// принимает на вход число и выдаёт количество 
+// цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
 
 
-Задача 30: Напишите программу, которая выводит 
-массив из 8 элементов, заполненный нулями и единицами 
-в случайном порядке.
-[1,0,1,1,0,1,0,0]
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-void CreateArray()
-{
-    int[] array = new int[8];
+// Console.WriteLine(GetCountNumber(num));
 
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(0, 2);
-        System.Console.Write(array[i] + " ");
-    }
-}
+// int GetCountNumber(int newNum)
+// {
+//     int count = 0;
 
-CreateArray();
+//     while (newNum > 0)
+//     {
+//         newNum = newNum / 10;
+//         count++;
+//     }
+
+//     return count;
+// }
+
+// Задача 28: Напишите программу, которая принимает
+//  на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int GetSum()
+// {
+//     int start = 1;
+//     int multiplication = 1;
+
+//     while (start <= num)
+//     {
+//         multiplication *= start;
+//         start++;
+//         // start = start+1;
+//     }
+//     return multiplication;
+// }
+
+// // int result = GetSum();
+// // System.Console.WriteLine(result);
+
+// Console.WriteLine(GetSum());
+
+
+// Задача 30: Напишите программу, которая выводит 
+// массив из 8 элементов, заполненный нулями и единицами 
+// в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// void CreateArray()
+// {
+//     int[] array = new int[8];
+
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0, 2);
+//         System.Console.Write(array[i] + " ");
+//     }
+// }
+
+// CreateArray();
